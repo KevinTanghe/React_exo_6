829341d45e81de73aa96ee12875511131abf7e5f
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-14 15:12:56 +0100

    Exo 9 finis

--- src/Component/HeadComponent/Navbar.js
+++ src/Component/HeadComponent/Navbar.js
@@ -1,14 +1,31 @@
 import './Navbar.css'
 import React from 'react'
+import { unstable_concurrentAct } from 'react-dom/test-utils'
+
+
 
 function Navbar() {
+
+
+    const Accueil = () => {
+        console.log("Accueil")
+    }
+
+    const Galerie = () => {
+        console.log("Galerie");
+    }
+
+    const Contact = () => {
+        console.log(("Contact"));
+    }
+
     return (
         <div className="containerNavbar">
             <nav>
                 <ul>
-                    <a>Link 1</a>
-                    <a>Link 2</a>
-                    <a>Link 3</a>
+                    <a onClick={ () => Accueil() }>Link 1</a>
+                    <a onMouseOver={ () => Galerie() }>Link 2</a>
+                    <a onDoubleClick={ () => Contact() }>Link 3</a>
                 </ul>
             </nav>
         </div>
@@ -16,3 +33,31 @@ function Navbar() {
 }
 
 export default Navbar
+
+// import './Navbar.css'
+// import React, { Component } from 'react'
+
+
+// export class Navbar extends Component {
+    
+//     Acceuil = () => {
+//         console.log("Acceuil");
+//     }
+
+
+//     render() {
+//         return (
+//             <div className="containerNavbar">
+//                 <nav>
+//                     <ul>
+//                         <a onClick={this.Acceuil}>Link 1</a>
+//                         <a>Link 2</a>
+//                         <a>Link 3</a>
+//                     </ul>
+//                 </nav>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Navbar;
